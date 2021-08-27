6384b3719fb55ecb50085034154457e7891d26c7
Oleg Usatyi <olegysik1@ukr.net>
2021-08-27 14:29:47 +0300

    add profile

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Profile from "./components/Profile/Profile";
+import user from "./user.json";
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        avatar={user.avatar}
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </div>
+  );
+}
